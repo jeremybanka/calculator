a21eaadd9d8ca5cb3f69e66fd62625c562b4b503
Jeremy Banka <jeremybanka@gmail.com>
2021-03-24 16:35:15 -0700

    add some extra notes on conditionals
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

---
+++ js/notes-on-conditions.js
@@ -0,0 +1,17 @@
+if(true) {
+  // this runs
+}
+
+const someStringVariable = '...'
+
+switch(someStringVariable) {
+  case 'somePossibility':
+    // this might run
+    break
+  case 'someOtherPossibility':
+    // this might run
+    break
+  default:
+    // if none of the possibilities above === someStringVariable
+    break
+}
