47f7d503f4423ec0166fcbe1bc34b5f8d3d2bb91
Jeremy Banka <jeremybanka@gmail.com>
2021-03-24 15:50:15 -0700

    add functionality for subtract, multiply, and divide
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- js/calc.js
+++ js/calc.js
@@ -16,13 +16,38 @@ function div(number1, number2) {
 }
 
 $(function() {
-  $("form#add").submit(function(e) {
+  
+  $("form#add").on('submit', function(e) {
     e.preventDefault()
     const number1 = parseInt($("#add1").val())
     const number2 = parseInt($("#add2").val())
     const result = add(number1, number2)
     const factStyle1 = number1 + " + " + number2 + " = " + result
     const factStyle2 = `${number1} + ${number2} = ${result}`
-    alert(factStyle2)
+    $('#add.output').text(result)
+  })
+
+  $("form#sub").on('submit', function(e) {
+    e.preventDefault()
+    const number1 = parseInt($("#sub1").val())
+    const number2 = parseInt($("#sub2").val())
+    const result = sub(number1, number2)    
+    $('#sub.output').text(result) 
+  })
+
+  $("form#mul").on("submit", function(e) {
+    e.preventDefault()
+    const number1 = parseInt($("#mul1").val())
+    const number2 = parseInt($("#mul2").val())
+    const result = mul(number1, number2)    
+    $('#mul.output').text(result) 
+  })
+
+  $("form#div").on("submit", function(e) {
+    e.preventDefault()
+    const number1 = parseInt($("#div1").val())
+    const number2 = parseInt($("#div2").val())
+    const result = div(number1, number2)    
+    $('#div.output').text(result) 
   })
 })
